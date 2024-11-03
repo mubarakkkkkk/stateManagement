9d7b2e9a10b5a67c9175105c31c7be2c45d1860f
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-11-03 13:30:47 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+  function addOne() {
+    setCount(count + 1)
+  };
+function NameList() {
+  const[list, setList] = useState(["jack","jill","john"]);
+  const[name, setName] = useState("");
+  const onAddName = () => {
+    list.push(name);
+  };
+  return (
+    <>
+      <button onClick={addOne}>count = {count}</button>
+      <ul>
+        {
+          list.map((name) => (
+            <li key={name}>
+                {name}
+            </li>
+          ))}
+      </ul>
+      <input type="text"
+      value={name}
+      onChange={(e) => setName(e.target.value)} />
+      <button onClick={onAddName}>Add Name</button>
+    </>
+  )
+}
+return(
+  <div>
+  <NameList />
+  </div>
+)
+ 
+}
+
+export default App
